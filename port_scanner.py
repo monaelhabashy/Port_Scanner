f411806355066f00cd887b6480bb29d376598041
Mona Elhabashy <monaellhabashy@gmail.com>
2026-08-05 18:17:47 -0400

    Implement port scanner with result logging
    This script scans a range of ports on a specified target IP or domain and saves the results to a file.

---
+++ port_scanner.py
@@ -0,0 +1,40 @@
+# port_scanner_project
+
+import socket
+
+# 1. get user input
+target = input("Enter target IP/Domain: ")
+start_port = int(input("Enter start port: "))
+end_port = int(input("Enter end port: "))
+
+print(f"\n[+] Starting scan on host: {target}")
+print(f"[+] Scanning range: {start_port} to {end_port}\n")
+
+# 2. open scan_results.txt to save the result
+with open("scan_results.txt", "w") as result_file:
+    result_file.write(f"--- Scan Results for {target} ---\n")
+
+    # 3. loop over every port in the range
+    for port in range(start_port, end_port + 1):
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # AF_INET: we use IPv4 addresses
+        # SOCK_STREAM: we use TCP protocol
+
+        s.settimeout(1.0)
+        # we set a timeout (one second) for each connection attempt
+
+        response = s.connect_ex((target, port))
+        # Attempts to establish a full TCP connection to the target and port
+        # Returns 0 if the connection succeeds (the port is open)
+        # and returns an error code (such as ECONNREFUSED) if the port is closed
+
+        if response == 0:
+            output_msg = f"[+] Port {port}: OPEN"
+            print(output_msg)
+            result_file.write(output_msg + "\n")
+
+        s.close()
+
+print("\n[+] Scan finished! Results saved to scan_results.txt")
+                                                                   
